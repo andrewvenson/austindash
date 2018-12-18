2bbafe2645361d19c6427b18dec3d54b1a69405f
unknown <vensoneandrew@gmail.com>
2018-12-18 11:58:18 -0600

    Added Subtotal feature

--- posDashboard/austinpos/static/JS/pricecart.js
+++ posDashboard/austinpos/static/JS/pricecart.js
@@ -1,14 +1,16 @@
 var tablerows = document.getElementById('tabletest').rows.length;
 var table = document.getElementById('tabletest');
 var cart = document.getElementById('cart');
+var subtotal = document.getElementById('subtotal')
 var jsonTest = '[]';
 var jsonObj = JSON.parse(jsonTest);
+var num = 0.00;
 
 for(x = 0; x < tablerows; x++){
-  table.rows[x].addEventListener('click', addTah);
+  table.rows[x].addEventListener('click', addCartItem);
 }
 
-function addTah(ev){
+function addCartItem(ev){
   index = this.rowIndex;
   equipmentCell = table.rows[index].cells[0];
   priceCell = table.rows[index].cells[1];
@@ -19,6 +21,8 @@ function addTah(ev){
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
 
+  cell1.style.width = '200px;'
+  cell2.style.width = '200px;'
   jsonObj[jsonObj.length] = {[equipmentName]:equipmentPrice};
 
   for(zz=0; zz < jsonObj.length; zz++){
@@ -28,7 +32,12 @@ function addTah(ev){
     cell2.innerHTML = "<strong>"+jsonObj[zz][equipmentName]+"</strong>";
     cell3.innerHTML = "<button class='btn btn-danger'>Delete</button>";
   }
+  console.log(equipmentPrice)
+  number = Number(equipmentPrice);
+  subtotal.innerHTML = num+=number;
 }
+
+
 
 
 
