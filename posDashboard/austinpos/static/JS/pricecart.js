1235e5f518079b25d6ee1ffcb22a1bab59e0f6e7
programmerprod <programmerprod@gmail.com>
2018-12-18 01:18:36 -0600

    Adding rows to cart from json data

--- posDashboard/austinpos/static/JS/pricecart.js
+++ posDashboard/austinpos/static/JS/pricecart.js
@@ -1,24 +1,17 @@
 var tablerows = document.getElementById('tabletest').rows.length;
 var table = document.getElementById('tabletest');
-
-var jsonTest = '[{"Epson":"65"}, {"Termnial":"65"}]';
+var cart = document.getElementById('cart');
+var jsonTest = '[]';
 var jsonObj = JSON.parse(jsonTest);
-console.log(jsonObj[0].Epson)
 
 for(x = 0; x < tablerows; x++){
   table.rows[x].addEventListener('click', addTah);
 }
 
 function addTah(ev){
-
-  jsonObj["whoah"] = "tah";
-  console.log(jsonObj);
-
-  var cart = document.getElementById('cart');
   index = this.rowIndex;
   equipmentCell = table.rows[index].cells[0];
   priceCell = table.rows[index].cells[1];
-  console.log(index);
   equipmentName = equipmentCell.innerHTML;
   equipmentPrice = priceCell.innerHTML;
   var row = cart.insertRow(0);
@@ -26,7 +19,17 @@ function addTah(ev){
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
 
-  cell1.innerHTML = equipmentName;
-  cell2.innerHTML = equipmentPrice;
-  cell3.innerHTML = "<button class='btn btn-danger'>Delete</button>"
+  jsonObj[jsonObj.length] = {[equipmentName]:equipmentPrice};
+
+  for(zz=0; zz < jsonObj.length; zz++){
+    for(key in jsonObj[zz]){
+      cell1.innerHTML = key;
+    }
+    cell2.innerHTML = "<strong>"+jsonObj[zz][equipmentName]+"</strong>";
+    cell3.innerHTML = "<button class='btn btn-danger'>Delete</button>";
+  }
 }
+
+
+
+
