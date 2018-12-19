ee7231f6d58754094f7b21a59ab19fa4233bee63
boredcpu <treyjermyn@gmail.com>
2018-12-19 11:56:42 -0600

    Made changes to dash

--- posDashboard/austinpos/static/JS/pricecart.js
+++ posDashboard/austinpos/static/JS/pricecart.js
@@ -1,16 +1,14 @@
 var tablerows = document.getElementById('tabletest').rows.length;
 var table = document.getElementById('tabletest');
 var cart = document.getElementById('cart');
-var subtotal = document.getElementById('subtotal')
 var jsonTest = '[]';
 var jsonObj = JSON.parse(jsonTest);
-var num = 0.00;
 
 for(x = 0; x < tablerows; x++){
-  table.rows[x].addEventListener('click', addCartItem);
+  table.rows[x].addEventListener('click', addTah);
 }
 
-function addCartItem(ev){
+function addTah(ev){
   index = this.rowIndex;
   equipmentCell = table.rows[index].cells[0];
   priceCell = table.rows[index].cells[1];
@@ -21,8 +19,6 @@ function addCartItem(ev){
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
 
-  cell1.style.width = '200px;'
-  cell2.style.width = '200px;'
   jsonObj[jsonObj.length] = {[equipmentName]:equipmentPrice};
 
   for(zz=0; zz < jsonObj.length; zz++){
@@ -32,12 +28,7 @@ function addCartItem(ev){
     cell2.innerHTML = "<strong>"+jsonObj[zz][equipmentName]+"</strong>";
     cell3.innerHTML = "<button class='btn btn-danger'>Delete</button>";
   }
-  console.log(equipmentPrice)
-  number = Number(equipmentPrice);
-  subtotal.innerHTML = num+=number;
 }
-
-
 
 
 
