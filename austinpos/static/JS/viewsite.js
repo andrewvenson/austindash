cfe6cbdad1933ee5e4cceaa2663179216fcef5d0
unknown <vensoneandrew@gmail.com>
2018-12-28 16:29:12 -0600

    Editing Message Box on sites page

--- austinpos/static/JS/viewsite.js
+++ austinpos/static/JS/viewsite.js
@@ -1,8 +1,18 @@
 var viewsitebutt = document.getElementById("viewsite");
 var siteinfo = document.getElementById("siteinfo");
-var messageBox = document.getElementById("message")
-var newmess = document.getElementById("buttmessage")
+var messageBox = document.getElementById("message");
+var newmess = document.getElementById("buttmessage");
+var specificsite = document.getElementById("sitemessage");
+var specificsitetext = document.getElementById("specificsitetextbox");
+var exitbutton = document.getElementById("exit");
 
+exitbutton.addEventListener('click', function(){
+  specificsitetext.style.display = "none";
+});
+
+specificsite.addEventListener('click', function(){
+  specificsitetext.style.display = "block";
+});
 
 newmess.addEventListener('click', function wow(){
   messageBox.style.display = "block";
