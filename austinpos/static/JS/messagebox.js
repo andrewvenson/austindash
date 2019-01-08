92cf0f5f5dc5d2bc2fb7fd8ecaadb9ad6c9771a0
Mr. ProudLegs <andrew@austintxpos.com>
2019-01-07 20:05:53 -0600

    Message testing

--- austinpos/static/JS/messagebox.js
+++ austinpos/static/JS/messagebox.js
@@ -35,70 +35,70 @@ socket.on('connect', function(){
 })
 
 
-var ticketform = $('#submitform').on('submit', function(e){
-  e.preventDefault()
-  var username = document.getElementById('un').innerHTML;
-  //DISPLAY MESSAGE BOX
+// var ticketform = $('#submitform').on('submit', function(e){
+//   e.preventDefault()
+//   var username = document.getElementById('un').innerHTML;
+//   //DISPLAY MESSAGE BOX
   
-  maximum.style.display = 'block';
+//   maximum.style.display = 'block';
 
-  $('#messages').append('<div>'+"<span style='color:black'>"+username+': '+'</span>' +$('#userticket').val()+'</div>')
+//   $('#messages').append('<div>'+"<span style='color:black'>"+username+': '+'</span>' +$('#userticket').val()+'</div>')
   
 
-  socket.emit('ticket', {
-    title : $('#issue').val(),
-    site : document.getElementById('sitename').innerHTML,
-    user : username, 
-    message: $('#messageinput').val()
-  })
-  $('#userticket').val('');
-  $('#messageinput').val('').focus()
-})
+//   socket.emit('ticket', {
+//     title : $('#issue').val(),
+//     site : document.getElementById('sitename').innerHTML,
+//     user : username, 
+//     message: $('#messageinput').val()
+//   })
+//   $('#userticket').val('');
+//   $('#messageinput').val('').focus()
+// })
 
 // MESSAGE BOX
-// var form = $('#form2').on('submit', function(e){
-//     e.preventDefault()
-//     var username = document.getElementById('un').innerHTML;
-//     if(document.getElementById('adminstatus').innerHTML == 'True'){
-//       socket.emit('private', {
-//           recipient: $('#recipient').val(),
-//           user : username,
-//           msg : $('#messageinput').val()
-//       })
-//       $('#messages').append('<div>'+"<span style='color:black'>"+username+': '+'</span>' +$('#messageinput').val()+'</div>')
-//       $('#messageinput').val('').focus()
-//     }else{
+var form = $('#form2').on('submit', function(e){
+    e.preventDefault()
+    var username = document.getElementById('un').innerHTML;
+    if(document.getElementById('adminstatus').innerHTML == 'True'){
+      socket.emit('private', {
+          recipient: $('#recipient').val(),
+          user : username,
+          msg : $('#messageinput').val()
+      })
+      $('#messages').append('<div>'+"<span style='color:black'>"+username+': '+'</span>' +$('#messageinput').val()+'</div>')
+      $('#messageinput').val('').focus()
+    }else{
       
-//       socket.emit('private', {
-//           user : username,
-//           msg : $('#messageinput').val()
-//       })
-//       $('#messages').append('<div>'+"<span style='color:black'>"+username+': '+'</span>' +$('#messageinput').val()+'</div>');
-//       $('#messageinput').val('').focus();
-//     }
+      socket.emit('private', {
+          user : username,
+          msg : $('#messageinput').val()
+      })
+      $('#messages').append('<div>'+"<span style='color:black'>"+username+': '+'</span>' +$('#messageinput').val()+'</div>');
+      $('#messageinput').val('').focus();
+    }
     
-// });
-
-// socket.on('privatemessages', function(message){
-//     console.log(message['msg'])
-//     if(document.getElementById('adminstatus').innerHTML == 'True'){
-//       $(document).ready(function(){
-//         $('#nomess').remove()
-//         $('#messages').append('<div>'+"<span style='color:black'>"+message['user']+': '+'</span>' + message['msg']+'</div>')
-//         console.log(message['user'], 'wow')
-//         document.getElementById('user').innerHTML = message['user'];
-//         // document.getElementById('queue').innerHTML = message['user'];
-//         $('#queue').append("<div class='border col-3' style='width:200px;height:200px;border-radius:5px; background-color:gray;'>" + message['user'] + "</div>")
-//       })
-//     }else{
-//       $('#nomess').remove()
-//         $('#messages').append('<div>'+"<span style='color:black'>"+message['user']+': '+'</span>' + message['msg']+'</div>');
-//         console.log(message['user'])
-//         document.getElementById('user').innerHTML = message['user'];
-//     }
-// });
-
-socket.on('privateticket', function(ticket){
-  // $('#messages').append('<div>'+"<span style='color:black'>"+message['user']+': '+'</span>' + message['msg']+'</div>');
-  $('#queue').append("<div class='border col-3' style='width:200px;height:200px;border-radius:5px; background-color:gray;'>" + ticket['site'] + "</div>")
 });
+
+socket.on('privatemessages', function(message){
+    console.log(message['msg'])
+    if(document.getElementById('adminstatus').innerHTML == 'True'){
+      $(document).ready(function(){
+        $('#nomess').remove()
+        $('#messages').append('<div>'+"<span style='color:black'>"+message['user']+': '+'</span>' + message['msg']+'</div>')
+        console.log(message['user'], 'wow')
+        document.getElementById('user').innerHTML = message['user'];
+        // document.getElementById('queue').innerHTML = message['user'];
+        $('#queue').append("<div class='border col-3' style='width:200px;height:200px;border-radius:5px; background-color:gray;'>" + message['user'] + "</div>")
+      })
+    }else{
+      $('#nomess').remove()
+        $('#messages').append('<div>'+"<span style='color:black'>"+message['user']+': '+'</span>' + message['msg']+'</div>');
+        console.log(message['user'])
+        document.getElementById('user').innerHTML = message['user'];
+    }
+});
+
+// socket.on('privateticket', function(ticket){
+//   // $('#messages').append('<div>'+"<span style='color:black'>"+message['user']+': '+'</span>' + message['msg']+'</div>');
+//   $('#queue').append("<div class='border col-3' style='width:200px;height:200px;border-radius:5px; background-color:gray;'>" + ticket['site'] + "</div>")
+// });
